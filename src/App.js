0cc8601b72b1da8b755a0a2903812a49b58cf0e9
Psarukoleksii <oleksiipsaruk@gmail.com>
2020-12-08 21:42:25 +0200

    'HomeWork4'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, {Component} from 'react';
-import Components from "./MyHomeWork/Homework1/Components";
-import Components2 from "./MyHomeWork/HomeWork2/Components2";
-import Components3 from "./MyHomeWork/HomeWork3/Components3";
-import Class1 from "./MyClassWork/Class1";
 
 class App extends Component {
     render() {
         return (
             <div>
-                {/******************HomeWork1********************/}
-                {/*<Components />*/}
-                {/******************HomeWork1********************/}
-
-                {/******************HomeWork2*********************/}
-                {/*<Components2 />*/}
-                {/*****************HomeWork2**********************/}
-
-                {/********************HomeWork3************************/}
-                {/*<Components3 />*/}
-                {/********************HomeWork3************************/}
-
-                <Class1 />
+                Hello world
             </div>
         );
     }
