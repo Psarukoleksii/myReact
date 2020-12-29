f687872d092ba8a7d5d3a43d2c012fc21fbb7955
Psarukoleksii <oleksiipsaruk@gmail.com>
2020-12-29 17:23:37 +0200

    'HomeWork7'

--- src/HomeWork7/redux/reducers/index.js
+++ src/HomeWork7/redux/reducers/index.js
@@ -1,1 +1,26 @@
-import {combineReducers} from "redux";
+import {decCounter, incCounter, randomCounter, resetCounter} from "../action-types";
+
+const initialState = {
+    counter: 0
+}
+export const reducer = (state = initialState, action) =>{
+    switch (action.type){
+        case incCounter:{
+            return {...state, counter: state.counter + 1}
+        }
+        case decCounter:{
+            return {...state, counter: state.counter - 1}
+        }
+        case resetCounter:{
+            return {...state, counter: 0}
+        }
+        case randomCounter:{
+            return {...state, counter: state.counter + action.payload}
+        }
+        default:{
+            return state;
+        }
+    }
+}
+
+export default reducer;
